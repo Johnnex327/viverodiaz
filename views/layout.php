c7a5ce70ff344613b945601afb387608e36a3d64
Jhon <jhon.naib327@gmail.com>
2022-06-29 16:39:06 -0500

    cincuentaseis commit

--- views/layout.php
+++ views/layout.php
@@ -12,7 +12,8 @@ if (!isset($inicio)) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vivero Diaz</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
