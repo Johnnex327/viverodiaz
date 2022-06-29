64f0aef26168795a8265c699a97d0fd99630ec20
Jhon <jhon.naib327@gmail.com>
2022-06-29 14:42:31 -0500

    cincuentados commit

--- Router.php
+++ Router.php
@@ -33,6 +33,7 @@ class Router
  */
         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
+        debuguear($currentUrl);
         
         if ($method === 'GET') {
             $fn = $this->getRoutes[$currentUrl] ?? null;
