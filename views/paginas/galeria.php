4e56cbaca8ad6c74921443ea36f78818ba2d8297
Jhon <jhon.naib327@gmail.com>
2022-10-16 01:07:02 -0500

    cambios2

--- views/paginas/galeria.php
+++ views/paginas/galeria.php
@@ -1,3 +1,14 @@
+<h1 class="h1-galeria"><span>UBICACIÓN DE LA EMPRESA “VIVERO DIAZ”</span></h1>
+
+<div class="contenedor img-gallery">
+    <img src="build/img/mapa.jpg" alt="">
+    <img src="build/img/mapa2.jpg" alt="">
+    <img src="build/img/mapa3.jpg" alt="">
+    <img src="build/img/mapa4.jpg" alt="">
+    <img src="build/img/mapa5.jpg" alt="">
+    
+</div>
+
 <h1 class="h1-galeria"><span>Galeria de Imagenes</span></h1>
 <div class=" contenedor img-gallery">
     <img src="build/img/1.jpg" alt="">
@@ -11,13 +22,3 @@
     <img src="build/img/9.jpg" alt="">
 </div>
 
-<h1 class="h1-galeria"><span>UBICACIÓN DE LA EMPRESA “VIVERO DIAZ”</span></h1>
-
-<div class="contenedor img-gallery">
-    <img src="build/img/mapa.jpg" alt="">
-    <img src="build/img/mapa2.jpg" alt="">
-    <img src="build/img/mapa3.jpg" alt="">
-    <img src="build/img/mapa4.jpg" alt="">
-    <img src="build/img/mapa5.jpg" alt="">
-    
-</div>
